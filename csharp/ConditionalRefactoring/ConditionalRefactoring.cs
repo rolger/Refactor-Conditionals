c06cc63085cbd045cb9295ccc057947c351e7c19
rolger <roland.germ@gmail.com>
2021-09-10 11:00:54 +0200

    feature: migrated code and tests to csharp

--- csharp/ConditionalRefactoring/ConditionalRefactoring.cs
+++ csharp/ConditionalRefactoring/ConditionalRefactoring.cs
@@ -1,7 +1,155 @@
-
 namespace ConditionalRefactoring
 {
     public class ConditionalRefactoring
     {
+        public static int Invert(int x)
+        {
+            if (x != 3)
+            {
+                return 3;
+            }
+
+            return 4;
+        }
+
+        public static int RedundantElse(int x)
+        {
+            if (x < 3)
+            {
+                return 1;
+            }
+
+            if (x < 10)
+            {
+                return 10;
+            }
+
+            if (x < 30)
+            {
+                return 30;
+            }
+
+            return 0;
+        }
+
+        public static bool DeMorgan(int x)
+        {
+            return !(x != 5 && x != 7);
+        }
+
+        public static int Join_AND(int x, int y)
+        {
+            if (x == 3)
+            {
+                if (y == 4)
+                {
+                    return x + y;
+                }
+            }
+
+            return 0;
+        }
+
+        public static int Split_AND(int x, int y)
+        {
+            if (x == 3 && y == 4)
+            {
+                return x + y;
+            }
+
+            return 0;
+        }
+
+        public static bool Join_OR(int x, int y)
+        {
+            if (x >= 0)
+            {
+                return true;
+            }
+
+            if (y <= 3)
+            {
+                return true;
+            }
+
+            return y == 10;
+        }
+
+        public static bool Split_OR(int x, int y)
+        {
+            if (x >= 0 || y <= 3 || y == 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static int Join_Statements(int x, int y)
+        {
+            var result = 0;
+            var factor = 1;
+            if (x > 3)
+            {
+                factor = x;
+            }
+
+            if (x > 3)
+            {
+                result += y * 3;
+            }
+
+            return result * factor;
+        }
+
+        public static int Split_Statements(int x, int y)
+        {
+            var result = 0;
+            var factor = 1;
+            if (x > 3)
+            {
+                result += y * 3;
+                factor = x;
+            }
+
+            return result * factor;
+        }
+
+        public static int Normalize(string s1, string s2)
+        {
+            if (!s1.Equals("hello"))
+            {
+                if (!s2.Equals("world"))
+                {
+                    if (!s1.Equals("foo"))
+                    {
+                        return 6;
+                    }
+
+                    return 1;
+                }
+
+                if (!s1.Equals("foo"))
+                {
+                    return 2;
+                }
+            }
+            else
+            {
+                if (s2.Equals("bar"))
+                {
+                    return 3;
+                }
+
+                if (!s2.Equals("world"))
+                {
+                    return 4;
+                }
+
+                return 5;
+            }
+
+            return 0;
+        }
     }
 }
--- csharp/ConditionalRefactoring/ConditionalRefactoringTest.cs
+++ csharp/ConditionalRefactoring/ConditionalRefactoringTest.cs
@@ -5,10 +5,111 @@ namespace ConditionalRefactoring
 {
     public class ConditionalRefactoringTest
     {
-        [Fact]
-        public void FailingTest()
+        [Theory]
+        [InlineData(3, 4)]
+        [InlineData(5, 3)]
+        [InlineData(1, 3)]
+        [InlineData(4, 3)]
+        public void Invert(int actual, int expected)
         {
-            "This test".Should().Be("failing");
+            ConditionalRefactoring.Invert(actual).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(7, 10)]
+        [InlineData(29, 30)]
+        [InlineData(30, 0)]
+        public void RedundantElse(int actual, int expected)
+        {
+            ConditionalRefactoring.RedundantElse(actual).Should().Be(expected);
+        }
+
+        [Fact]
+        public void DeMorgan()
+        {
+            ConditionalRefactoring.DeMorgan(5).Should().BeTrue();
+            ConditionalRefactoring.DeMorgan(7).Should().BeTrue();
+
+            ConditionalRefactoring.DeMorgan(4).Should().BeFalse();
+            ConditionalRefactoring.DeMorgan(8).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(4, 3, 0)]
+        [InlineData(7, 3, 0)]
+        [InlineData(1, 3, 0)]
+        [InlineData(3, 4, 7)]
+        [InlineData(3, 11, 0)]
+        public void Join_AND(int x, int y, int expected)
+        {
+            ConditionalRefactoring.Join_AND(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(4, 3, 0)]
+        [InlineData(7, 3, 0)]
+        [InlineData(1, 3, 0)]
+        [InlineData(3, 4, 7)]
+        [InlineData(3, 11, 0)]
+        public void Split_AND(int x, int y, int expected)
+        {
+            ConditionalRefactoring.Split_AND(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0, 3, true)]
+        [InlineData(3, 6, true)]
+        [InlineData(-1, 3, true)]
+        [InlineData(-1, 5, false)]
+        [InlineData(-1, 10, true)]
+        public void Split_OR(int x, int y, bool expected)
+        {
+            ConditionalRefactoring.Split_OR(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0, 3, true)]
+        [InlineData(3, 6, true)]
+        [InlineData(-1, 3, true)]
+        [InlineData(-1, 5, false)]
+        [InlineData(-1, 10, true)]
+        public void Join_OR(int x, int y, bool expected)
+        {
+            ConditionalRefactoring.Join_OR(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(4, 3, 36)]
+        [InlineData(7, 3, 63)]
+        [InlineData(1, 3, 0)]
+        [InlineData(3, 4, 0)]
+        public void Split_Statements(int x, int y, int expected)
+        {
+            ConditionalRefactoring.Split_Statements(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(4, 3, 36)]
+        [InlineData(7, 3, 63)]
+        [InlineData(1, 3, 0)]
+        [InlineData(3, 4, 0)]
+        public void Join_Statements(int x, int y, int expected)
+        {
+            ConditionalRefactoring.Join_Statements(x, y).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("foo", "world", 0)]
+        [InlineData("foo", "bar", 1)]
+        [InlineData("bar", "world", 2)]
+        [InlineData("hello", "bar", 3)]
+        [InlineData("hello", "foo", 4)]
+        [InlineData("hello", "world", 5)]
+        [InlineData("bar", "foo", 6)]
+        public void Normalize(string s1, string s2, int expected)
+        {
+            ConditionalRefactoring.Normalize(s1, s2).Should().Be(expected);
         }
     }
 }
